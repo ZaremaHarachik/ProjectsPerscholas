57c9476f5cdc0c9f070c5fb0ff1309d9328fe883
bigbird <harachikzarema@gmail.com>
2023-06-01 22:57:03 -0700

    sss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react';
+
+export default function Form() {
+    const [form, setForm] = useState({
+        firstName: 'Barbara',
+        lastName: 'Hepworth',
+        email: 'bhepworth@sculpture.com',
+    });
+
+    return (
+        <>
+            <label>
+                First name:
+                <input
+                    value={form.firstName}
+                    onChange={e => {
+                        setForm({
+                            ...form,
+                            firstName: e.target.value
+                        });
+                    }}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={form.lastName}
+                    onChange={e => {
+                        setForm({
+                            ...form,
+                            lastName: e.target.value
+                        });
+                    }}
+                />
+            </label>
+            <label>
+                Email:
+                <input
+                    value={form.email}
+                    onChange={e => {
+                        setForm({
+                            ...form,
+                            email: e.target.value
+                        });
+                    }}
+                />
+            </label>
+            <p>
+                {form.firstName}{' '}
+                {form.lastName}{' '}
+                ({form.email})
+            </p>
+        </>
+    );
 }
-
-export default App;
---
+++ src/Components/NavbarButton/NavbarButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const NavbarButton = ({ text }) => {
+    return (
+        <a href="#">{text}</a>
+    );
+}
+
+export default NavbarButton;
